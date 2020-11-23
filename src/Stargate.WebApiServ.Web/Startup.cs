14dcddd038d71f794c4b63a2b732a385fd29a6ad
朱俊明 <libra.zhu@hotmail.com>
2020-11-23 18:56:10 +0800

    添加全局异常捕获，以堵塞未处置的未明异常。

--- src/Stargate.WebApiServ.Web/Startup.cs
+++ src/Stargate.WebApiServ.Web/Startup.cs
@@ -1,8 +1,15 @@
+using System;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Serilog;
 
@@ -20,7 +27,21 @@ namespace Stargate.WebApiServ.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    var jsonConfig = Configuration.GetSection("Json");
+                    if (jsonConfig.GetValue<string>("NamingPolicy").Equals("CamelCase", StringComparison.OrdinalIgnoreCase))
+                    {
+                        options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+                        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                    }
+                    if (jsonConfig.GetValue<bool>("RelaxedEscaping", defaultValue: true))
+                        options.JsonSerializerOptions.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+                    options.JsonSerializerOptions.IgnoreNullValues = jsonConfig.GetValue<bool>("IgnoreNullValues", defaultValue: true);
+                    options.JsonSerializerOptions.WriteIndented = jsonConfig.GetValue<bool>("WriteIndented", defaultValue: false);
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Stargate.WebApi", Version = "v1" });
@@ -28,7 +49,7 @@ namespace Stargate.WebApiServ.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
@@ -38,7 +59,23 @@ namespace Stargate.WebApiServ.Web
             }
             else
             {
-                app.UseExceptionHandler("/error");
+                app.UseExceptionHandler(builder => builder.Run(async context =>
+                {
+                    var feature = context.Features.Get<IExceptionHandlerPathFeature>();
+                    var resp = new
+                    {
+                        Message = feature?.Error?.Message ?? "全局异常捕获了一个未被处置的未明异常。",
+                        Path = feature?.Path,
+                        Source = feature?.Error?.Source ?? "未指明异常抛出源。"
+                    };
+                    
+                    logger.LogError(feature?.Error, message: resp.Message);
+
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var jsonString = JsonSerializer.Serialize(resp);
+                    await context.Response.WriteAsync(jsonString);
+                }));
             }
 
             app.UseHttpsRedirection();
